1e95f51f74925daf638d56ac7aa637b21afd921a
fbrcode <fabio.bressler@gmail.com>
2022-07-04 19:55:50 -0300

    define named accounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,6 +24,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.8",
+  defaultNetwork: "hardhat",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
@@ -37,5 +38,10 @@ module.exports = {
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
   },
 };
